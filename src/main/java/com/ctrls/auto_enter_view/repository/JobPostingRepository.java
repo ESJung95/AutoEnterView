980f2132c0e0b6cd82c747337db99d0a4d18cac0
IM-GYURI <kgulr0517@gmail.com>
2024-07-08 16:47:04 +0900

    feat : add findByJobPostingKey, findAllByCompanyKey

--- src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
@@ -1,8 +1,13 @@
 package com.ctrls.auto_enter_view.repository;
 
 import com.ctrls.auto_enter_view.entity.JobPostingEntity;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
 
+  Optional<JobPostingEntity> findByJobPostingKey(String jobPostingKey);
+
+  List<JobPostingEntity> findAllByCompanyKey(String companyKey);
 }
