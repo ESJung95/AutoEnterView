3d6034faccddb338c576bef74096720b5ad704aa
IM-GYURI <kgulr0517@gmail.com>
2024-07-08 16:44:37 +0900

    feat : add getJobPostingsByCompanyKey
    회사 본인이 등록한 채용공고 목록 조회 기능 추가

--- src/main/java/com/ctrls/auto_enter_view/controller/JobPostingController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/JobPostingController.java
@@ -1,7 +1,12 @@
 package com.ctrls.auto_enter_view.controller;
 
+import com.ctrls.auto_enter_view.dto.jobPosting.JobPostingDto;
 import com.ctrls.auto_enter_view.service.JobPostingService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,4 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobPostingController {
 
   private final JobPostingService jobPostingService;
+
+  /**
+   * 회사 본인이 등록한 채용공고 목록 조회
+   *
+   * @param companyKey
+   * @return
+   */
+  @GetMapping("/companies/{companyKey}/posted-job-postings")
+  public ResponseEntity<List<JobPostingDto>> getJobPostingsByCompanyKey(
+      @PathVariable String companyKey) {
+    return ResponseEntity.ok(jobPostingService.getJobPostingsByCompanyKey(companyKey));
+  }
 }
